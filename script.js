8ab83f82f74efac1558f0d18be0a6f03108d9c10
GitMulC <cmulvo@gmail.com>
2024-04-19 23:36:53 +0000

    Add comments & bold txt

--- script.js
+++ script.js
@@ -2,26 +2,23 @@ let numbers2 = [];
 let size = 0;
 let average = 0;
 
+//core logic to get the average and sum of array
 function findAverage(arr) { 
     let sum = 0; 
     for (let i = 0; i < arr.length; i++) { 
       sum += arr[i]; 
     } 
-    average = sum / arr.length; 
-    console.log(sum);
+    average = sum / arr.length;
     document.getElementById('sum').innerHTML += sum;
-    console.log(average);
     document.getElementById('average').innerHTML += average;
   } 
 
-
+//generates the array created on the site by the user
 function startArray(num){
     size = document.getElementById('user').value;
-    console.log(size);
     for (let i=0;i<size;i++){
         numbers2[i] = parseInt(prompt("Please enter a number:"));
     }
-    console.log(numbers2);
     document.getElementById('array').innerHTML += numbers2;
 }
 
