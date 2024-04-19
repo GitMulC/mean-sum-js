0b4620c504578cdabda28c04d20f1806a2aef060
GitMulC <cmulvo@gmail.com>
2024-04-19 23:30:58 +0000

    Style pg & add mean sum logic in script.js

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
+let numbers2 = [];
+let size = 0;
+let average = 0;
+
 function findAverage(arr) { 
     let sum = 0; 
     for (let i = 0; i < arr.length; i++) { 
       sum += arr[i]; 
     } 
-    return sum / arr.length; 
+    average = sum / arr.length; 
+    console.log(sum);
+    document.getElementById('sum').innerHTML += sum;
+    console.log(average);
+    document.getElementById('average').innerHTML += average;
   } 
-   
-  let numbers = [1, 2, 3, 4, 5]; 
-  let average = findAverage(numbers); 
-  console.log(average); // This will output 3
+
+
+function startArray(num){
+    size = document.getElementById('user').value;
+    console.log(size);
+    for (let i=0;i<size;i++){
+        numbers2[i] = parseInt(prompt("Please enter a number:"));
+    }
+    console.log(numbers2);
+    document.getElementById('array').innerHTML += numbers2;
+}
+
